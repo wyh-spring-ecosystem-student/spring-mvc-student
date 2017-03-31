829cedefc2bef546ebd68258ab8c456bae0f9bfe
yuhao.wang@msxf.com <xiaolyuh@163.com>
2017-03-31 17:13:54 +0800

    Spring MVC静态资源配置

--- src/main/java/com/chenfeng/xiaolyuh/config/MvcConfig.java
+++ src/main/java/com/chenfeng/xiaolyuh/config/MvcConfig.java
@@ -4,19 +4,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 /**
  * MVC 配置类。这里我们配置了一个jsp的ViewResolver，用来映射路径和实际页面的位置，
- * 其中@EnableWebMvc注解会开启一些默认的配置，如一些ViewResolver或者MessageConverter
+ * 其中@EnableWebMvc注解会开启一些默认的配置，如一些ViewResolver或者MessageConverter。
+ * 
+ * Spring MVC的定制配置需要我们的配置类继承WebMvcConfigurerAdapter类（Adapter适配器），
+ * 并在此类加上@EnableWebMvc注解，来开启对Spring MVC的配置支持，这样我们就可以重写这个类的方法，
+ * 来完成我们的配置。如果不加@EnableWebMvc注解，重写这些方法也无效。
  * @author yuhao.wang
  * @Date 2017年3月29日 下午3:41:20
  */
 @Configuration
-@EnableWebMvc
+@EnableWebMvc// 开启对Spring MVC的支持，如果不加@EnableWebMvc注解，重写这些方法也无效。
 @ComponentScan("com.chenfeng.xiaolyuh")
-public class MvcConfig {
+public class MvcConfig extends WebMvcConfigurerAdapter {// 重写WebMvcConfigurerAdapter类的方法可以对Spring MVC
 	
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
@@ -25,5 +31,11 @@ public class MvcConfig {
 		viewResolver.setSuffix(".jsp");
 		viewResolver.setViewClass(JstlView.class);
 		return viewResolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// addResourceLocations是指文件放置的目录，addResourceHandler是指对外暴露的地址
+		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
 	}
 }
